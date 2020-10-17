a8ec5d8169c3eff5a257c8afffa8c6bbf3f55fc2
Qdea <58660439+Qdea@users.noreply.github.com>
2020-10-17 13:48:41 +0300

    Create task7.sh

---
+++ lab2/task7.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+(for i in $(ls /proc | grep "[0-9]")
+do
+awk '$1 == "rchar" {print "%d ", $2}' /proc/$i/io >> term1.info
+cmd=$(ps -e -o pid,cmd | awk -v id=$i '$1 == id {print $2}')
+echo $i $cmd >> term1.info
+done
+sleep 60
+for i in $(ls /proc | grep "[0-9]")
+do
+awk '$1 == "rchar" {print "%d ", $2}' /proc/$i/io >> term2.info
+echo $i >> term2.info
+done
+while read str
+do
+pid=$(echo $str | awk '{print $2}')
+mem=$(echo $str | awk '{print $1}')
+awk -v id=$pid -v m=$mem '{if ($2 == id) {printf "PID %d : CMD %s : Memory %d ", $2, $3, m-$1; print "\n"}}' term1.info >> term3.info
+done < term2.info
+sort -n -k 8 | tail -n 3 | sort -n -k 8
+rm *.info
+) 2> /dev/null
